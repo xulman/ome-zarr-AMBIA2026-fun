b8dc4d86ec646d9e03ced1014077f6f3ec7ea7c9
Vladimír Ulman <ulman@fi.muni.cz>
2026-08-31 23:02:41 +0200

    COS,CHG: S2 writes 3 different time points

--- step2_time_contribute_chunk.py
+++ step2_time_contribute_chunk.py
@@ -3,10 +3,7 @@ import ngff_zarr as nz
 import step1_time_create_empty_skeleton as S1
 
 
-def main():
-    # THIS IS THE TIME POINT TO BE FILLED
-    T = 2
-
+def write_whole_timepoint(time_index: int, base_value = 100):
     # read the skeleton/reference OME-Zarr, and get path to the base-level image
     multiscales = nz.from_ngff_zarr(S1.file_path)
 
@@ -21,8 +18,8 @@ def main():
     # example: write progressively full time point into a buffer at base/full-resolution
     buf = np.zeros(images[0].shape[1:], dtype=images[0].dtype)
     # ... fill buf progressively as data arrives ...
-    buf[ 0:32,:,:] = 128.1
-    buf[32:64,:,:] = 182.1
+    buf[ 0:32,:,:] = base_value
+    buf[32:64,:,:] = base_value+1
 
     # turn 'buf' into a "one-time" multiscales (that would be copied into the original dataset)
     base_image = nz.to_ngff_image(buf, dims=multiscales.metadata.dimension_names[1:])
@@ -35,7 +32,13 @@ def main():
     # publish: one full time point
     # (...benefiting that the write to a store happens immediately)
     for level in range(len(images)):
-        images[level][T] = tmp_multiscales.images[level].data
+        images[level][time_index] = tmp_multiscales.images[level].data
+
+
+def main():
+    write_whole_timepoint(1, 30)
+    write_whole_timepoint(3, 50)
+    write_whole_timepoint(4, 60)
 
 
 if __name__ == "__main__":
