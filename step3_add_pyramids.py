ae4c273ab223fffb5adc4fd3b18ac04c3a213c3e
Vladimír Ulman <ulman@fi.muni.cz>
2026-08-31 14:19:39 +0200

    (BUG)FIX: S3.adding pyramids was dropping coordSys/Trans from the original metadata

--- step3_add_pyramids.py
+++ step3_add_pyramids.py
@@ -1,16 +1,22 @@
 import ngff_zarr as nz
+import dataclasses
 import step1_create_empty_skeleton as S1
 
 
 def main():
     # read the skeleton/reference OME-Zarr, and its base image fully
-    multiscales = nz.from_ngff_zarr(S1.file_path)
-    base_image = multiscales.images[0]
+    ms_orig = nz.from_ngff_zarr(S1.file_path)
 
-    # re-do the metadata again, notice that it is basically the same params like in S1,
-    # except that more scale factors are added; similarly below for the to_ome_zarr()
-    multiscales = nz.to_multiscales(base_image, scale_factors=[2,4], chunks=S1.chunks)
-    nz.to_ome_zarr(S1.file_path, multiscales, version="0.6", overwrite=False, start_level=1)
+    # re-do the metadata again, by building a fresh new multiscales object that
+    # pulls in the pyramids/scales and which will blend with the original metadata
+    ms_built = nz.to_multiscales(ms_orig.images[0], scale_factors=[2, 4], chunks=S1.chunks)
+
+    start = 1  # keep levels < start from disk, append levels >= start
+    merged_datasets = ms_orig.metadata.datasets[:start] + ms_built.metadata.datasets[start:]
+    merged_metadata = dataclasses.replace(ms_orig.metadata, datasets=merged_datasets)
+    ms_spliced = dataclasses.replace(ms_built, metadata=merged_metadata, root_attributes=ms_orig.root_attributes)
+
+    nz.to_ome_zarr(S1.file_path, ms_spliced, version="0.6", overwrite=False, start_level=1)
 
 
 if __name__ == "__main__":
